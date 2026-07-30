993c4c5a7d3eb70834225c613589bfa5bb729f9d
kseniiautorina-lang <kseniia.utorina@gmail.com>
2026-07-31 01:54:23 +0300

    Анимация на логотип

--- script.js
+++ script.js
@@ -55,3 +55,43 @@
   window.addEventListener("scroll", setHeaderState, { passive: true });
   setHeaderState();
 })();
+
+initShinyText();
+
+function initShinyText() {
+  var nodes = document.querySelectorAll("[data-shiny]");
+  if (!nodes.length) return;
+
+  nodes.forEach(function (el) {
+    var speed = Number(el.dataset.speed || 2);
+    var delay = Number(el.dataset.delay || 0);
+    var color = el.dataset.color || "#b5b5b5";
+    var shineColor = el.dataset.shineColor || "#ffffff";
+    var spread = Number(el.dataset.spread || 120);
+    var pauseOnHover = el.dataset.pauseOnHover === "true";
+    var disabled = el.dataset.disabled === "true";
+
+    el.style.setProperty("--shiny-color", color);
+    el.style.setProperty("--shiny-shine", shineColor);
+    el.style.setProperty("--shiny-spread", spread + "deg");
+    el.style.setProperty("--shiny-speed", speed + "s");
+    el.style.setProperty("--shiny-delay", delay + "s");
+
+    if (disabled) {
+      el.style.animation = "none";
+      el.style.backgroundImage = "none";
+      el.style.color = color;
+      el.style.webkitTextFillColor = color;
+      return;
+    }
+
+    if (pauseOnHover) {
+      el.addEventListener("mouseenter", function () {
+        el.classList.add("is-paused");
+      });
+      el.addEventListener("mouseleave", function () {
+        el.classList.remove("is-paused");
+      });
+    }
+  });
+}
